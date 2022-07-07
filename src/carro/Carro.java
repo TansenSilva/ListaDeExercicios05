24ec63153c2573726c21ba497417ef10b2923df2
TansenSilva <tansensilva@gmail.com>
2022-07-07 15:59:28 -0300

    Envio de todas as respostas da lista de número 05.

--- src/carro/Carro.java
+++ src/carro/Carro.java
@@ -6,17 +6,25 @@ public class Carro {
     String modelo;
     String marca;
 
-    public Carro(int quantidadeDePortas, double potencia, String modelo, String marca) {
+    boolean ligaCarro;
+
+    public Carro(int quantidadeDePortas, double potencia, String modelo, String marca,boolean ligaCarro) {
         this.quantidadeDePortas = quantidadeDePortas;
         this.potencia = potencia;
         this.modelo = modelo;
         this.marca = marca;
+        this.ligaCarro = false;
     }
 
     public Carro(int quantidadeDePortas, String modelo, String marca) {
         this.quantidadeDePortas = quantidadeDePortas;
         this.modelo = modelo;
         this.marca = marca;
+    }
+
+    public void ligarCarro(){
+        this.ligaCarro = true;
+        System.out.println("O carro está ligado: "+ligaCarro);
     }
 
     public Carro() {
--- src/carro/MainCarro.java
+++ src/carro/MainCarro.java
@@ -2,11 +2,9 @@ package carro;
 
 public class MainCarro {
     public static void main(String[] args) {
-        Carro carro1 = new Carro();
-        carro1.marca = "Fiat";
-        carro1.modelo = "Uno";
-        carro1.quantidadeDePortas = 5;
-        System.out.println("Especificações do Veículo\nMarca: "+carro1.marca + "\nModelo: "+carro1.modelo+ "\nQuantidade de Portas: "+carro1.quantidadeDePortas+"\nPotência: "+carro1.potencia);
+        Carro carro1 = new Carro(4,1.0,"gol","Volks",false);
+
+        carro1.ligarCarro();
 
         Carro carro2 = new Carro(5,"Fusca","Volks");
         System.out.println(carro2.potencia);
