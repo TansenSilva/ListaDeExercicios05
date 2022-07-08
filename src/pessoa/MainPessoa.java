e96ab145be8e1bc1530fbfeeea3e6cf8adc90959
TansenSilva <tansensilva@gmail.com>
2022-07-08 10:09:52 -0300

    Envio de todas as respostas da lista de número 05.

--- src/pessoa/MainPessoa.java
+++ src/pessoa/MainPessoa.java
@@ -1,15 +1,20 @@
 package pessoa;
 
+import java.util.Scanner;
+
 public class MainPessoa {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Pessoa pessoa = new Pessoa("Tansen",33,1.75);
         Pessoa pessoa2 = new Pessoa("Bruce",25,87,1.85);
         Pessoa pessoa3 = new Pessoa("Zé",18,1.58);
 
         System.out.println("o peso do "+pessoa2.nome+" é: "+pessoa2.peso);
-        pessoa2.emagrecer(10);
+        System.out.print("Digite o quanto você emagreceu: ");
+        pessoa2.emagrecer(input.nextDouble());
         System.out.println("Seu novo peso é: "+pessoa2.peso);
-        pessoa2.engordar(8);
+        System.out.print("Digite o quanto você emagreceu: ");
+        pessoa2.engordar(input.nextDouble());
         System.out.println("Seu novo peso é: "+pessoa2.peso);
         System.out.println("a idade do "+pessoa2.nome+" é: "+pessoa2.idade);
         pessoa2.envelhecer();
